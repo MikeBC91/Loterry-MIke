d3fec148712e68cf5f6e9bcda76436cad795d46a
Michael Briceno <162876022+MikeBC91@users.noreply.github.com>
2024-05-16 08:30:54 +0000

    fix

--- obj/Debug/net8.0/Loterry-Mike.AssemblyInfo.cs
+++ obj/Debug/net8.0/Loterry-Mike.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Loterry-Mike")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+378034cc0278c7f0fcc66638b0816a1a9726644e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dd80833a42491ba9372bdfa0d6d4823c15d7aef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Loterry-Mike")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Loterry-Mike")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
